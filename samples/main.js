74ad0a2681114ab7274e761f2624fbb7df3a7815
dimaspirit <dspwww@gmail.com>
2016-11-17 19:55:05 +0200

    continue work

--- samples/main.js
+++ samples/main.js
@@ -1,53 +1,104 @@
 /* JSHint inline rules */
 /* jshint node: true, browser: true */
-/* globals qbMediaRecorder */
+/* globals qbMediaRecorder, Promise */
 
 'use strict';
-var mediaEl = document.querySelector('.j-video_local');
+// var mediaEl = document.querySelector('.j-video_local');
 
-function getTypeMedia() {
-    var radioButtons = document.getElementsByName('type');
-    for (var i = 0; i < radioButtons.length; i++) {
-        if (radioButtons[i].checked) return radioButtons[i].value;
+var streamCard = {
+    stream: null,
+    mediaEl: null,
+    recorder: null,
+    typeStream: null, // 1 - audio only, 2 - audion & video
+    isRecording: false,
+    init: function() {
+        var self = this;
+
+        return new Promise(function(resolve, reject) {
+            self.typeStream =  self.getChoosesTypeMedia();
+            self.mediaEl = document.querySelector('.j-video_local');
+
+            self.setupListeners();
+
+            self.getMediaStreamLocal().then(function(stream) {
+                self.stream = stream;
+                resolve(stream);
+            }).catch(function(error) {
+                reject(error);
+            });
+        });
+        
+    },
+    getChoosesTypeMedia: function() {
+        var radioButtons = document.getElementsByName('type');
+        
+        for (var i = 0; i < radioButtons.length; i++) {
+            if (radioButtons[i].checked) {
+                return radioButtons[i].value;
+            }
+        }
+        
+        return undefined;
+    },
+    getMediaStreamLocal: function() {
+        var constraints = {
+                audio: true,
+                video: this.getChoosesTypeMedia() === '1' ? false : true
+            };
+
+        return new Promise(function(resolve, reject) {
+            navigator.mediaDevices.getUserMedia(constraints)
+                .then(function(stream) {
+                    resolve(stream);
+                })
+                .catch(function(error) {
+                    reject(error);
+                });
+        });
+    },
+    attachStreamToSource: function() {
+        console.info(this.mediaEl);
+        this.mediaEl.pause();
+        this.mediaEl.src='';
+
+        this.mediaEl.src = URL.createObjectURL(this.stream);
+        this.mediaEl.play();
     }
-    return '';
-}
+};
 
-function handleGetStreamError(error) {
-    console.error(error.name + ': ' + error.message, error);
-}
+streamCard.init().then(function() {
+    streamCard.attachStreamToSource();
+});
 
-function handleGetStreamSuccess(stream) {
-    /* stop previously stream */
-    mediaEl.pause();
-    mediaEl.src="";
+
+
+// function handleGetStreamError
+
+// function handleGetStreamSuccess(stream) {
+//     /* stop previously stream */
+//     
     
-    mediaEl.src = URL.createObjectURL(stream);
-    mediaEl.play();
-}
+//     
+// }
 
-function getMediaStreamLocal() {
-    var constraints = {
-        audio: true,
-        video: getTypeMedia() === '1' ? false : true
-    };
+// function getMediaStreamLocal() {
+    
+// }
 
-    navigator.mediaDevices.getUserMedia(constraints)
-        .then(handleGetStreamSuccess)
-        .catch(handleGetStreamError);
-}
+// getMediaStreamLocal();
 
-getMediaStreamLocal();
+// /** set events listeners */
+// var radioButtons = document.getElementsByName('type');
 
-/** set events listeners */
-var radioButtons = document.getElementsByName('type');
-
-for (var i = 0; i < radioButtons.length; i++) {
-    radioButtons[i].addEventListener('change', getMediaStreamLocal, false);
-}
+// for (var i = 0; i < radioButtons.length; i++) {
+//     radioButtons[i].addEventListener('change', getMediaStreamLocal, false);
+// }
 
 
-
+    // handleGetStreamSuccess: function(stream) {
+    //     console.info(this);
+    //     
+    // },
 
 
 // var recorder = null,
